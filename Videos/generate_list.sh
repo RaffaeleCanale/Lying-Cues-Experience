19228517de6cf571922382a5f8bea182069e14ba
Canale <raffaele.canale@epfl.ch>
2016-01-23 23:37:52 +0100

    Updated generate_list.sh

--- Videos/generate_list.sh
+++ Videos/generate_list.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+#	1: Directory
+#	2: video type ('lying', 'truth', 'training')
+function processDirectory() {
+	for file in "$1"/*; do
+		local filename=$(basename "$file")
+		local extension="${filename##*.}"
+		if $transcode; then
+			extension="mp4"
+		fi
+
+		local name="image$i.$extension"
+
+		copy "$file" "$output/$name"
+		echo "$name : $2 : $filename" >> "$namesMapFile"
+
+		i=$((i+1))
+	done
+}
+
 #  	1: Input file
 #	2: Output file
-#	3: Enable transcode
 function copy() {
-	if $3; then
+	if $transcode; then
 		avconv -i "$1" -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=480:480 -threads 0 -an "$2"
 	else
 		cp "$1" "$2"
 	fi
 }
-
-#	1: Video name
-#	2: Video type ('lying', 'truth', 'training')
-#	3: Original file name
-function echoFileInfo() {
-	echo "$1 : $2 : $3"
-}
-
 
 if [ ! -d "Lies" ] || [ ! -d "Truths" ] || [ ! -d "Training" ]; then
 	echo "Place all your truths videos in a directory called 'Truth', the lies in 'Lies' and training videos in 'Training'"
@@ -41,26 +51,6 @@ if [ -f "$namesMapFile" ]; then
 fi
 
 i=1;
-for file in Lies/*; do
-	name="video$i.mp4"
-
-	copy "$file" "$output/$name" $transcode
-	echoFileInfo "$name" "lying" "$file" >> "$namesMapFile"
-	i=$((i+1))
-done
-
-for file in Truths/*; do
-	name="video$i.mp4"
-
-	copy "$file" "$output/$name" $transcode
-	echoFileInfo "$name" "truth" "$file" >> "$namesMapFile"
-	i=$((i+1))
-done
-
-for file in Training/*; do
-	name="video$i.mp4"
-
-	copy "$file" "$output/$name" $transcode
-	echoFileInfo "$name" "training" "$file" >> "$namesMapFile"
-	i=$((i+1))
-done
+processDirectory "Lies" "lying"
+processDirectory "Truths" "truth"
+processDirectory "Training" "training"
